692beff9106e1efaa75f2d4de47b7ace4f6640d4
Sujith Kumar <sujith.kumar83@outlook.com>
2018-11-08 13:47:27 +0530

    First Commit

---
+++ src/IPPSUS_2.11.4.7.scala
@@ -0,0 +1,17 @@
+
+
+
+import io.StdIn._
+
+println("Welcome to Happy House Cafe")
+println("Enter the number of Hamburgers")
+val hb=readInt()
+println("Enter the number of French Fries")
+val ff=readInt()
+println("Enter the number of milk shakes")
+val ms=readInt()
+println("Happy House Cafe")
+println(s"The total Amount post tax is ${((hb*2+ff*1+ms*3)*1.08)}")
+
+
+
